7601993493b7d260b9ea01346ac3fc7460bedf67
Tomatoguy <122667069+Tomatoguy1029@users.noreply.github.com>
2024-06-19 18:01:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = this;
+  member.skills = [];
+  member.addSkill = function (skill) {
+    member.skills.push(skill);
+  };
+  member.removeSkill = function (skill) {
+    member.skills = member.skills.filter(function (s) {
+      return s !== skill;
+    });
+  };
+}
